d96ccfda7773afb4be442bae2d3e4715db96f5f2
Huascar Oña <huascarking@hotmail.com>
2013-02-19 20:10:09 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urlshare::Application.config.secret_token = 'b7b69e7d9bacf6a7f778aa056177fe8a62d29f8d6052a207228779ed86073b4bf421994cb7d157090b5e6c764fe36eb2a1b465ebb048a83b5e55f70fd076a66e'
