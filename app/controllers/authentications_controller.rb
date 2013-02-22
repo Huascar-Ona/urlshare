a210947e0010a948d5035c94bc619decfa7f9574
Huascar Oña <huascarking@hotmail.com>
2013-02-21 22:20:07 -0600

    new authentications controller

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,12 @@
+class AuthenticationsController < ApplicationController
+  def fb_tab
+    app_id = "477773122285636"
+    app_id_secret = "cf1c67c394a576cba3d01a7911372017"
+    @oauth = Koala::Facebook::OAuth.new(app_id, app_id_secret)
+    @signed = params[:signed_request]
+    @fb_request = @oauth.parse_signed_request(@signed)
+    @oauth_access_token = @fb_request["oauth_token"]
+    @graph = Koala::Facebook::API.new(@oauth_access_token)
+    @profile = @graph.get_object("me")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Urlshare::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  match '/fb_tab' => 'authentications#fb_tab', method: :post
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
